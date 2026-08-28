4a8fe508ce7db8a088e597c085d33239af643d07
AlsuSharapova <alsusharapova1011@gmail.com>
2026-08-28 11:53:27 +0400

    Work with the database and add a migration

---
+++ TechStore/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechStore.Models.Entities;
+
+namespace TechStore.Data {
+    public class AppDbContext : DbContext{
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<ProductSpecification> ProductSpecifications { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(10, 2);
+        }
+    }
+}
