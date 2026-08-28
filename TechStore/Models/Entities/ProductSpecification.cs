55f72141ec88fc3e072d1339a15910c3101f143d
AlsuSharapova <alsusharapova1011@gmail.com>
2026-08-28 11:30:15 +0400

    Add Entities to project

---
+++ TechStore/Models/Entities/ProductSpecification.cs
@@ -0,0 +1,10 @@
+﻿namespace TechStore.Models.Entities {
+    public class ProductSpecification {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = null!;
+    }
+}
