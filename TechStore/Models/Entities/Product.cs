e61b63c1f6fa9da654c88cdfe5aff42ad9ecc7c0
AlsuSharapova <alsusharapova1011@gmail.com>
2026-08-28 12:03:01 +0400

    Add IsBestSeller to Products and update database

--- TechStore/Models/Entities/Product.cs
+++ TechStore/Models/Entities/Product.cs
@@ -6,6 +6,7 @@
         public decimal Price { get; set; }
         public int StockQuantity { get; set; }
         public string ImageUrl { get; set; } = string.Empty;
+        public bool IsBestSeller { get; set; } = false;
 
         public int CategoryId { get; set; }
         public Category Category { get; set; } = null!;
