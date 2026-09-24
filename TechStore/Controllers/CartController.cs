fca624cdf22a07700b07c23bac881522cb34e5f2
AlsuSharapova <alsusharapova1011@gmail.com>
2026-09-24 19:49:49 +0400

    `Implement full cart functionality`

---
+++ TechStore/Controllers/CartController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TechStore.Data;
+using TechStore.Models.Entities;
+
+namespace TechStore.Controllers {
+    [Authorize]
+    public class CartController : Controller {
+        private readonly AppDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CartController(AppDbContext context, UserManager<ApplicationUser> userManager) {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index() {
+            var userId = _userManager.GetUserId(User);
+
+            var cart = await _context.Carts
+                .Include(c => c.Items)
+                    .ThenInclude(i => i.Product)
+                .FirstOrDefaultAsync(c => c.UserId == userId);
+
+            return View(cart);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(int productId, int quantity = 1) {
+            var userId = _userManager.GetUserId(User);
+
+            var cart = await _context.Carts
+                .Include(c => c.Items)
+                .FirstOrDefaultAsync(c => c.UserId == userId);
+
+            if (cart == null) {
+                cart = new Cart { UserId = userId! };
+                _context.Carts.Add(cart);
+                await _context.SaveChangesAsync();
+            }
+
+            var existingItem = cart.Items.FirstOrDefault(i => i.ProductId == productId);
+
+            if (existingItem != null) {
+                existingItem.Quantity += quantity;
+            }
+            else {
+                _context.CartItems.Add(new CartItem {
+                    CartId = cart.Id,
+                    ProductId = productId,
+                    Quantity = quantity
+                });
+            }
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateQuantity(int itemId, int quantity) {
+            var userId = _userManager.GetUserId(User);
+
+            var item = await _context.CartItems
+                .Include(i => i.Cart)
+                .Include(i => i.Product)
+                .FirstOrDefaultAsync(i => i.Id == itemId && i.Cart.UserId == userId);
+
+            if (item == null) {
+                return NotFound();
+            }
+
+            if (quantity < 1) {
+                quantity = 1;
+            }
+
+            if (quantity > item.Product.StockQuantity) {
+                quantity = item.Product.StockQuantity;
+            }
+
+            item.Quantity = quantity;
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Remove(int itemId) {
+            var userId = _userManager.GetUserId(User);
+
+            var item = await _context.CartItems
+                .Include(i => i.Cart)
+                .FirstOrDefaultAsync(i => i.Id == itemId && i.Cart.UserId == userId);
+
+            if (item == null) {
+                return NotFound();
+            }
+
+            _context.CartItems.Remove(item);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TechStore/Models/Entities/Cart.cs
@@ -0,0 +1,8 @@
+﻿namespace TechStore.Models.Entities {
+    public class Cart {
+        public int Id { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public ApplicationUser User { get; set; } = null!;
+        public ICollection<CartItem> Items { get; set; } = new List<CartItem>();
+    }
+}
